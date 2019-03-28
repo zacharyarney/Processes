adad9920b208e4ca1939e53f6824e4716d98a401
zacharyarney <zachary.arney@gmail.com>
2019-03-28 15:56:54 -0700

    ex2 folowed instructions, but getting error on execution

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -1,14 +1,28 @@
-// Write a program that opens the text.txt file (with the `fopen()` library call) located in this directory 
-// and then calls `fork()` to create a new process. Can both the child and parent access the file descriptor 
-// returned by `fopen()`? What happens when they are written to the file concurrently?
+// Write a program that opens the text.txt file (with the `fopen()` library
+// call) located in this directory and then calls `fork()` to create a new
+// process. Can both the child and parent access the file descriptor returned
+// by `fopen()`? What happens when they are written to the file concurrently?
 
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 
+// This code gets a segmentation fault 11 and doesn't write in the file
+// I'm not totally sure what's causing the error
+// seg fault: 11 suggests I'm trying to write info somewhere I don't
+// have access to but I'm not writing to memory
+// tried with different fopen modes too with no success...
 int main(void)
 {
-    // Your code here 
+    // create file pointer
+    FILE *fp;
+    // open file
+    fopen("text.txt", "w");
+    fork();
+    // write to file
+    fprintf(fp, "Maybe we're writing this twice?\n");
+    // close file
+    fclose(fp);
     
     return 0;
 }
